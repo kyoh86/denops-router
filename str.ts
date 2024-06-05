160783edbb67ed2c6c25400e642fc9b9577b48e0
kyoh86 <me@kyoh86.dev>
2024-06-05 23:57:12 +0900

    format

--- str.ts
+++ str.ts
@@ -1,7 +1,10 @@
 function titleCase(str: string) {
-  return str.replaceAll(/(?<=^|\W+)\w/g, (s) => s.toUpperCase()).replaceAll(/\W/g, "");
+  return str.replaceAll(/(?<=^|\W+)\w/g, (s) => s.toUpperCase()).replaceAll(
+    /\W/g,
+    "",
+  );
 }
 
 export function commandName(...words: string[]) {
-  return words.map(titleCase).join("")
+  return words.map(titleCase).join("");
 }
--- str_test.ts
+++ str_test.ts
@@ -1,9 +1,9 @@
-import {commandName} from "./str.ts";
+import { commandName } from "./str.ts";
 import { assertEquals } from "https://deno.land/std@0.210.0/assert/mod.ts";
 
 Deno.test("commandName", () => {
-  assertEquals(commandName("foo", "bar/baz"), "FooBarBaz")
-  assertEquals(commandName("foo", "bar/baz/qux"), "FooBarBazQux")
-  assertEquals(commandName("foo", "bar///baz"), "FooBarBaz")
-  assertEquals(commandName("foo", "bar/b/az"), "FooBarBAz")
-})
+  assertEquals(commandName("foo", "bar/baz"), "FooBarBaz");
+  assertEquals(commandName("foo", "bar/baz/qux"), "FooBarBazQux");
+  assertEquals(commandName("foo", "bar///baz"), "FooBarBaz");
+  assertEquals(commandName("foo", "bar/b/az"), "FooBarBAz");
+});
