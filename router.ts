8547390f5897484a3a8a16ae935ce9c23d9970a0
kyoh86 <me@kyoh86.dev>
2025-01-01 01:48:30 +0900

    fix: batch augroup-autocmd commands

--- router.ts
+++ router.ts
@@ -145,12 +145,17 @@ export class Router {
         await option.swapfile.setBuffer(denops, abuf, false);
         await option.modified.setBuffer(denops, abuf, false);
         if (handler.save) {
-          await denops.cmd(`
-            augroup denops-${denops.name}-${this.#scheme}-saver-${abuf}
-            autocmd! *
-            autocmd BufWriteCmd <buffer> call denops#request('${denops.name}', '${prefix}:internal:save', [${abuf}, '${afile}'])
-          `);
-          await option.buftype.setBuffer(denops, abuf, "acwrite");
+          await batch(denops, async (denops) => {
+            await denops.cmd(
+              `augroup denops-${denops.name}-${this.#scheme}-saver-${abuf}`,
+            );
+            await denops.cmd(`autocmd! *`);
+            await denops.cmd(
+              `autocmd BufWriteCmd <buffer> call denops#request('${denops.name}', '${prefix}:internal:save', [${abuf}, '${afile}'])`,
+            );
+            await denops.cmd(`augroup END`);
+            await option.buftype.setBuffer(denops, abuf, "acwrite");
+          });
         } else {
           await option.modifiable.setBuffer(denops, abuf, false);
           await option.readonly.setBuffer(denops, abuf, true);
